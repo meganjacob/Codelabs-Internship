d4eeb56b2fd3f8f0b52b7d9dc251058f000814e5
EC2 Default User <ec2-user@ip-172-31-59-107.ec2.internal>
2020-07-12 21:36:49 +0000

    Testing git

--- src/utilities.py
+++ src/utilities.py
@@ -4,3 +4,5 @@ import dask.dataframe as dd
 pandas_df = pd.read_pickle("./raw_weekly_df.pkl")
 dask_df = dd.from_pandas(pandas_df, npartitions=8)
 
+#Downcast the dataframe
+pandas_df.info(memory_usage="Deep")
