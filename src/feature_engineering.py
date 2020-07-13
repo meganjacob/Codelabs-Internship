bef08bfab0aa099a221ca55c920598c583644686
EC2 Default User <ec2-user@ip-172-31-59-107.ec2.internal>
2020-07-13 21:55:15 +0000

    added two statistical features in feature_engineering, and showed examples in feature_engineering.ipynb

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -5,13 +5,25 @@ import pandas as pd
 def standardize(a):
   astd = np.std(a)
   mean = a.mean()
-  a -= a.mean()
-  a /= astd
-  return a,mean,astd
+  b = a.copy()
+  b -= b.mean()
+  b /= astd
+  return b,mean,astd
 
 #Unstandarizes a series, given it's mean and std.
 def unstandardize(a,mean,astd):
     a = a*astd
     a = a + mean
     return a
+
+def stat_feature(df1,grouping,col):
+    df = df1.copy()
+    mean = df.groupby([grouping])[col].mean()
+    mi = df.groupby([grouping])[col].min()
+    ma = df.groupby([grouping])[col].max()
+    return mean, mi, ma
+
+def agg_feature(df1,grouping,col):
+    df = df1.copy()
+    return df.groupby([grouping])[col].cumsum()
 
