14aec0838a77aecaa77eb1f59c17b28d16b8ad3a
EC2 Default User <ec2-user@ip-172-31-59-107.ec2.internal>
2020-07-19 20:41:44 +0000

    finished some last TODO's for the functions written last week.

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,28 +1,31 @@
 #Given a pandas series, this returns the standarized series(in z-score space).
 #TODO what if you want to standardize multiple columns? is creating global variables a safe way to maintain state?
-def standardize(input_ser):
-    input_std = np.std(input_ser)
-    input_mean = input_ser.mean()
-    global mean
-    global std
-    mean = input_mean
-    std = input_std
-    
-    return (input_ser-input_mean)/input_std
+stat = dict()
+def standardize(df,col):
+    input_std = np.std(df[col])
+    input_mean = df[col].mean()
+    global stat
+    stat[col] = [input_std,input_mean]
+    return (df[col]-input_mean)/input_std
 
 
 
 #Unstandarizes a series. 
-def unstandardize(ser):
-    return ser*std + mean
+def unstandardize(ser,col):
+    global stat
+    return ser*stat[col][0] + stat[col][1]
 
 
 #Gives two dataframes. The first one has the stats mean, min, and max, and the second one has the cumulative sums.
 #TODO don't hardcode! what if you only want the mean?
-def stat_feature(df,grouping,col):
-    agg = df.groupby([grouping])[col].agg(['mean','min','max'])
+#feature has to be an array filled with strings of the desired features, except cumsum -- if you want cumsum, please use the cum_stat function instead. An example of features is ['mean','min','max']
+def stat_feature(df,grouping,col,features):
+    agg = df.groupby([grouping])[col].agg(features)
+    return agg
+
+def cum_stat(df,grouping,col):
     cumsum = df.groupby([grouping])[col].shift(periods = 1).agg(['cumsum'])
-    return agg,cumsum 
+    return cumsum
 
 
 #Detrends a dataframe, given the grouping, and column. 
